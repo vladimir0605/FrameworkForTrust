83e2e924e3cf3027f26b26f99b57e616d7b3acb7
vladimir0605 <vladimir.djukic0605@gmail.com>
2026-07-14 12:22:15 +0200

    Add files via upload

---
+++ frontend/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import  { debugQuadrantData,getQuadrantData } from "./web3Config";
+import { WalletProvider } from "./WalletContext"; // ✅
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <WalletProvider>
+      <App />
+    </WalletProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+// debugQuadrantData(1);
+
+// Za DEBUGGING - ukloniti prije produkcije:
+window.getQuadrantData = getQuadrantData;
+
+// Moze se postaviti umjesto gornje window.get.. linije:
+//if (process.env.NODE_ENV === 'development') {
+  //window.getQuadrantData = getQuadrantData;
+//}
+
